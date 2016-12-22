3d60f8ba0e2151ebe12ab64f6ae9231cdb5be103
Peter Murphy <peterkmurphy@gmail.com>
2016-12-22 10:29:15 +1000

    5.7 to 5.8 checked - Chapter 5 complete (if a little suspect)

--- app.js
+++ app.js
@@ -60,4 +60,16 @@ app.controller('PostsCtrl', [
 'posts',
 function($scope, $stateParams, posts){
     $scope.post = posts.posts[$stateParams.id];
+
+    $scope.addComment = function(){
+      if($scope.body === '') { return; }
+      $scope.post.comments.push({
+        body: $scope.body,
+        author: 'user',
+        upvotes: 0
+      });
+      $scope.body = '';
+    };
+
+
 }]);
