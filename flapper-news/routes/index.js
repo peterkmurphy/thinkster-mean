7298459dfff14031b4451c3e4b9dbfc24140456e
Peter Murphy <peterkmurphy@gmail.com>
2016-12-22 22:18:27 +1000

    8.11 to 8.13 ticked - Chapter 8 complete

--- flapper-news/routes/index.js
+++ flapper-news/routes/index.js
@@ -40,8 +40,24 @@ router.param('post', function(req, res, next, id) {
   });
 });
 
-router.get('/posts/:post', function(req, res) {
-  res.json(req.post);
+router.param('comment', function (req, res, next, id) {
+	var query = Comment.findById(id);
+
+	query.exec(function (err, comment) {
+		if (err) { return next(err); }
+		if (!comment) { return next(new Error("can't find comment")); }
+
+		req.comment = comment;
+		return next();
+	});
+});
+
+router.get('/posts/:post', function(req, res, next) {
+  req.post.populate('comments', function(err, post) {
+    if (err) { return next(err); }
+
+    res.json(post);
+  });
 });
 
 router.put('/posts/:post/upvote', function(req, res, next) {
@@ -49,6 +65,32 @@ router.put('/posts/:post/upvote', function(req, res, next) {
     if (err) { return next(err); }
 
     res.json(post);
+  });
+});
+
+router.put('/posts/:post/comments/:comment/upvote', function (req, res, next) {
+	req.comment.upvote(function (err, comment) {
+		if (err) {
+			return next(err);
+		}
+
+		res.json(comment);
+	});
+});
+
+router.post('/posts/:post/comments', function(req, res, next) {
+  var comment = new Comment(req.body);
+  comment.post = req.post;
+
+  comment.save(function(err, comment){
+    if(err){ return next(err); }
+
+    req.post.comments.push(comment);
+    req.post.save(function(err, post) {
+      if(err){ return next(err); }
+
+      res.json(comment);
+    });
   });
 });
 
