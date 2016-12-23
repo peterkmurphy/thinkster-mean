43175d637a07b273fb9f8d20f1289ab6680e2774
Peter Murphy <peterkmurphy@gmail.com>
2016-12-23 11:51:27 +1000

    11.1 to 11.7 ticked - Chapter 11 finished

--- flapper-news/app.js
+++ flapper-news/app.js
@@ -1,8 +1,11 @@
 var mongoose = require('mongoose');
 require('./models/Posts');
 require('./models/Comments');
+require('./models/Users');
 mongoose.connect('mongodb://localhost/news');
 
+var passport = require('passport');
+require('./config/passport');
 var express = require('express');
 var path = require('path');
 var favicon = require('serve-favicon');
@@ -29,7 +32,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
-
+app.use(passport.initialize());
 app.use('/', index);
 app.use('/users', users);
 
